7f8c4065fa69cce37a1023260ce2bfc7d0e01f10
sarthak264 <sarthaksharm372@gmail.com>
2021-10-08 22:07:54 +0530

    done

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("word count app");
+// DOM elements
+const input = document.getElementById("input");
+const count = document.querySelector("#count span");
+
+// variables
+let words = 0;
+let array = [];
+
+const countWords = () => {
+  array = input.value.split(/\s/);
+  array[array.length - 1] === "" && array.pop();
+  count.textContent = array.length;
+};
+
+input.addEventListener("input", countWords);
